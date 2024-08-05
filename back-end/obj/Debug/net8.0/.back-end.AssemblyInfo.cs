7255386df910169fce495921fa58949de68c6698
HUUQUY1004 <99793142+HUUQUY1004@users.noreply.github.com>
2024-08-06 02:13:05 +0700

    Nhúng sql

--- back-end/obj/Debug/net8.0/.back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/.back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0899b5d2d62d63ea3d96923893fcfb864ab4efb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586c09819e5e3ec3dad1f9603a5f616c2c651a8d")]
 [assembly: System.Reflection.AssemblyProductAttribute(".back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
