c044e22a9db8832af83a621744eef58131440f22
HUUQUY1004 <99793142+HUUQUY1004@users.noreply.github.com>
2024-08-18 01:12:46 +0700

    Add Doctor

--- back-end/obj/Debug/net8.0/.back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/.back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute(".back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff6a6a7b30f560ff12b56b939b92351582545244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb618c8a614f3ce58f20a3500948370665cbb420")]
 [assembly: System.Reflection.AssemblyProductAttribute(".back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute(".back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
